5e3cd4be0577a7f2950cb866ba98cbc10aad6d60
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2023-12-29 23:09:51 +0530

    Day-05/75 github challenge (gfg)

---
+++ Day5.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+ using namespace std;
+
+ int kSubstrConcat (int n, string s, int k)
+	{
+	    // Your Code Here
+	    if(n%k != 0){
+	        return 0;
+	    }
+	   
+	  unordered_map<string,int> m;
+	  for(int i=0; i<n; i+=k){
+	      m[s.substr(i,k)]++;
+	      if(m.size() > 2) 
+	      return 0;
+	  }
+	  if(m.size() < 2) 
+	  return 1;
+	  
+	  for(auto it = m.begin(); it!= m.end(); ++it){
+	      if(it->second == 1) 
+	      return 1;
+	  }
+	  return 0;
+	}
+
+int main(){
+         int t; cin >> t;
+    while (t--)
+	{
+		int n; cin >> n;
+		string s; cin >> s;
+		int k; cin >> k;
+   
+		cout << kSubstrConcat (n, s, k) << endl;
+	}
+return 0;}
